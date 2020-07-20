cc12689afcc1dcb32fd1744a8adec6197e0a8ed9
Lizz <lizwesterband@gmail.com>
2020-07-20 15:02:00 -0700

    [javascript] Add DOM manipulation to README]

--- README.md
+++ README.md
@@ -16,3 +16,45 @@ const music ={
     },
 }
 ```
+
+
+## Examples of Functions
+```javascript
+//standard function
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+//function expression
+const multiplyNumbers = function(num1, num2){
+    return num1 * num2;
+};
+
+//arrow function
+const subtractNumbers = (num1, num2) =>{
+    return num1 - num2;
+}
+```
+
+## DOM Manipulation
+
+```javascript
+/telling the dom to find the div with a class of container
+const container= document.querySelector(".container");
+console.log(container);
+//create a new header within hte document; create new element
+const headerTwo = document.createElement("h2");
+headerTwo.textContent = "My first JS review";
+console.log(headerTwo);
+//add this to the document without adding in html
+container.appendChild(headerTwo);
+
+//add a class to headerTwo
+
+headerTwo.classList.add("sub-title");
+headerTwo.setAttribute("class", "header-two");
+console.log(headerTwo);
+//take away a class
+headerTwo.classList.remove ("sub-title");
+console.log(headerTwo)
+
+```
--- app.js
+++ app.js
@@ -59,3 +59,66 @@ console.log(friends)
 console.log(tesla)
 //second part of line 19 20
 tesla.print();
+
+//this function takes an array and uses for each 
+// to name a new variable and print each
+//  one of that variable in an array
+function printFriends(array){
+    array.forEach(friend =>{
+        // //array.forEach(function(element){
+
+        // })
+        console.log(friend);
+    });
+}
+
+function printName(element){
+    console.log(element)
+}
+
+friends.forEach(function(friend){
+    printName(friend);
+});
+
+printFriends(friends);
+//use the same function to print multiple things
+printFriends(tesla.foundingMembers)
+
+
+
+//standard function
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+//function expression
+const multiplyNumbers = function(num1, num2){
+    return num1 * num2;
+};
+
+//arrow function
+const subtractNumbers = (num1, num2) =>{
+    return num1 - num2;
+};
+
+
+//DOM document object model
+//telling the dom to find the div with a class of container
+const container= document.querySelector(".container");
+console.log(container);
+//create a new header within hte document; create new element
+const headerTwo = document.createElement("h2");
+headerTwo.textContent = "My first JS review";
+console.log(headerTwo);
+//add this to the document without adding in html
+container.appendChild(headerTwo);
+
+//add a class to headerTwo
+//when used together, the last one overrides the first
+
+headerTwo.classList.add("sub-title", //adding this in will make both classes"header-Two"
+);
+headerTwo.setAttribute("class", "header-two");
+console.log(headerTwo);
+//take away a class
+headerTwo.classList.remove ("sub-title");
+console.log(headerTwo)
